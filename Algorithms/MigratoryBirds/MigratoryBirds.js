dfe864f31b0ede971dc064be6029ed95b014b5a3
Kevin <leeyhteachi@gmail.com>
2018-04-09 17:54:44 -0400

    migratorybirds

---
+++ Algorithms/MigratoryBirds/MigratoryBirds.js
@@ -0,0 +1,18 @@
+//input: array of integers representing individual sightings of species of birds
+//  species are represented by the integers 1-5. Example input: [1,4,4,4,5,3]
+//output: integer representing the most sighted species, favoring species with lower ID #
+
+function migratoryBirds(n, ar) {
+    // Complete this function
+    const birds = [1,2,3,4,5];
+    const count = birds.map((n) => ar.reduce( (a, m) => (m === n) ? a + 1 : a, 0));
+    return birds[count.reduce((a, n, i, arr) => {
+        if ( n > a.count ){
+            a.index = i;
+            a.count = n
+            return a;
+        } else {
+            return a;
+        }
+    }, { index: 0, count: 0 }).index];
+}
